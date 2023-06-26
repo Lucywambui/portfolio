431d3169c7e150bdb3a0ba0ebda56b69d94c08b9
Lucywambui <lmbugua63.lm@gmail.com>
2023-06-26 07:51:22 +0300

    (style)(ui) navbar and home page components

--- portfolio/projects/pages/src/lib/pages.module.ts
+++ portfolio/projects/pages/src/lib/pages.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 @NgModule({
   declarations: [
 
+  
+    NavbarComponent,
+        HomepageComponent
   ],
   imports: [
   ],
