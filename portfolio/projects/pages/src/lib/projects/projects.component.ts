36de8cad8e18bae9d2c726fe7f5409697e5e099f
Lucywambui <lmbugua63.lm@gmail.com>
2023-07-01 10:57:58 +0300

    Update projects.component.ts

--- portfolio/projects/pages/src/lib/projects/projects.component.ts
+++ portfolio/projects/pages/src/lib/projects/projects.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'lib-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
 
